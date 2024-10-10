8c03794a2c875c40c53c356b936b38e8e1642dd3
david <nz1985@qq.com>
2024-10-10 22:41:08 +0800

    新增：thinkphp验证助手函数

--- src/helpers.php
+++ src/helpers.php
@@ -6,6 +6,8 @@ use Ledc\Snowflake\RedisSequenceResolver;
 use Ledc\Snowflake\Snowflake;
 use RedisException;
 use support\Redis;
+use think\exception\ValidateException;
+use think\Validate;
 
 /**
  * 雪花ID生成器
@@ -18,7 +20,40 @@ function snowflake(): Snowflake
     $sequence->setCachePrefix('Snowflake:' . md5(__FILE__) . ':');
     $snowflake = new Snowflake;
     $snowflake->setSequenceResolver($sequence);
+
     return $snowflake;
+}
+
+/**
+ * thinkPHP验证器助手函数
+ * @param array $data 待验证的数据
+ * @param array|string $validate 验证器类名或者验证规则数组
+ * @param array $message 错误提示信息
+ * @param bool $batch 是否批量验证
+ * @return bool|true
+ * @throws ValidateException
+ */
+function validate(array $data, array|string $validate, array $message = [], bool $batch = false): bool
+{
+    if (is_array($validate)) {
+        $v = new Validate();
+        $v->rule($validate);
+    } else {
+        if (strpos($validate, '.')) {
+            // 支持场景
+            [$validate, $scene] = explode('.', $validate);
+        }
+        if (!class_exists($validate)) {
+            throw new ValidateException('验证类不存在:' . $validate);
+        }
+        /** @var Validate $v */
+        $v = new $validate();
+        if (!empty($scene)) {
+            $v->scene($scene);
+        }
+    }
+
+    return $v->message($message)->batch($batch)->failException(true)->check($data);
 }
 
 /**
@@ -31,9 +66,9 @@ function current_git_commit(string $branch = 'master', bool $short = true): stri
     if (is_file($filename)) {
         $hash = file_get_contents($filename);
         $hash = trim($hash);
-
         return $short ? substr($hash, 0, 7) : $hash;
     }
+
     return '';
 }
 
@@ -48,5 +83,6 @@ function current_git_filemtime(string $branch = 'master', string $format = 'Y-m-
         $time = filemtime($filename);
         return date($format, $time);
     }
+
     return '';
 }
